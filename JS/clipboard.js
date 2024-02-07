f9aa84f61a5fcd7f6b0c0c59ee56d024537e53a5
Daniel Shaw <danielshaw71@gmail.com>
2024-02-07 17:59:27 +0000

    Fixed links to go to new tab, and clickable area of links reduced

--- JS/clipboard.js
+++ JS/clipboard.js
@@ -1,11 +1,16 @@
 // Copy Email To Clipboard
 
-let text = document.getElementById('myEmail').innerHTML;
 const copyToClipboard = async () => {
   try {
+    let text = document.getElementById('myEmail').innerHTML;
     await navigator.clipboard.writeText(text);
     console.log('Email copied to clipboard');
+    document.querySelector('.tooltiptext').innerHTML = 'Copied To Clipboard';
   } catch (err) {
     console.error('Failed to copy: ', err);
   }
 }
+
+document.querySelector('#myEmail').addEventListener('mouseleave', () => {
+  document.querySelector('.tooltiptext').innerHTML = 'Copy To Clipboard';
+});
