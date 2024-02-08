fcd24aa1b18ad037a222f11f2abc91211da3e8df
Daniel Shaw <danielshaw71@gmail.com>
2024-02-08 18:06:47 +0000

    Finished media queries for main styling, need to add queries for navbar and maybe circles

--- JS/scroll.js
+++ JS/scroll.js
@@ -4,14 +4,14 @@ const body = document.querySelector('body');
 const navbar = document.getElementById('navbar-container');
 const dropdown = document.querySelector('.dropdown-content');
 
-// window.addEventListener('scroll', () => {
-//   if (window.scrollY >= 0 && window.scrollY < 700) {
-//     body.style.backgroundColor = '#1A191D';
-//     navbar.style.backgroundColor = '#1A191D';
-//     navbar.style.opacity = '0';
-//   } else {
-//     body.style.backgroundColor = 'rgba(26, 25, 29, 0.95)';
-//     navbar.style.backgroundColor = '#252428';
-//     navbar.style.opacity = '0.9';
-//   }
-// });
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 0 && window.scrollY < 700) {
+    body.style.backgroundColor = '#1A191D';
+    navbar.style.backgroundColor = '#1A191D';
+    navbar.style.opacity = '0';
+  } else {
+    body.style.backgroundColor = 'rgba(26, 25, 29, 0.95)';
+    navbar.style.backgroundColor = '#252428';
+    navbar.style.opacity = '0.9';
+  }
+});
